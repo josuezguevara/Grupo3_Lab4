a7dc2b1afe125d8e86d6bb764ff11c76298d5a74
Josuezguevara <josuezelaya99@hotmail.es>
2017-05-12 16:12:23 -0600

    Correcciones

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include "Pieza.h"
+#include "Marine.h"
 #include <iostream>
+
 
 void printMatriz(int, Pieza**);
 
 using namespace std;
 
 int main(){
-  Pieza*** Tablero[][] = new Pieza**[8]
+  Pieza*** Tablero = new Pieza**[8];
   for(int i = 0; i < 8; i++){
      Tablero[i] = new Pieza*[8];
   }
   for(int i = 0; i < 8; i++){
      for(int j = 0; j < 8; j++){
-	if((i == 0 && j == 1) || (i == 0 4&& j == 3) || (i == 0 && j == 5) || (i == 0 && j == 7)){
-	   Tablero ->  Marine('B');
+	if((i == 0 && j == 1) || (i == 0 && j == 3) || (i == 0 && j == 5) || (i == 0 && j == 7)){
+	   Tablero[i][j] = new Marine('B');
 	} else if ((i == 1 && j == 0) || (i == 1 && j == 2) || (i == 1 && j == 4) || (i == 1 && j == 6)) {
 
 	} else if ((i == 2 && j == 1) || (i == 2 && j == 3) || (i == 2 && j == 5) || (i == 2 && j == 7)) {
@@ -29,7 +31,6 @@ int main(){
 	}
      }
   }
-  printMatriz(8, )
   
   return 0;
 }
@@ -37,16 +38,8 @@ int main(){
 void printMatriz(int size, Pieza** matrix){
         for(int i = 0; i < size; i++){
                 for(int j = 0; j < size; j++) {
-                         cout << "  " << matrix[i][j];
+                         cout << "  " << "algo";
                 }
                 cout << endl;
         }
 }
-
-<<<<<<< HEAD
-int main(){
-  
-=======
->>>>>>> 98064cfce42416f5d6e52f14f13b4310ed8f5d66
-
-
