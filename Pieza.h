bc3399a4aeed9c44bd4bb77b4cd8aa6b5c65fe0d
Josuezguevara <josuezelaya99@hotmail.es>
2017-05-12 13:44:52 -0600

    Crando Figura.h

---
+++ Pieza.h
@@ -0,0 +1,23 @@
+#ifndef PIEZA_H
+#define PIEZA_H
+
+class Pieza{
+ 
+ private:
+  int X;
+  int Y;
+
+ public:
+  Pieza();
+  Pieza(int,int);
+  
+  void setX(int);
+  int getX();
+  void setY(int);
+  int getY();
+
+
+
+
+};
+#endif
